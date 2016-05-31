64537dd23e54e3162a0aef66be744455d8eee9f9
Otavio <otavioensa@gmail.com>
2016-05-30 22:44:36 -0300

    Final version

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
 
-	var dweetClient = require('node-dweetio');
+  var dweetClient = require('node-dweetio');
   var dweetio = new dweetClient();
-	var five = require('johnny-five');
-	var board = new five.Board();
+  var five = require('johnny-five');
+  var board = new five.Board();
 
-	board.on('ready', function () {
+  board.on('ready', function () {
 
-		// Definimos uma variação de 3 graus para enviar o valor para a Dashboard
-		var tempSensor = new five.Sensor({
-			pin: "A0",
-			threshold: 4
-		});
+    var tempSensor = new five.Sensor({
+      pin: "A0",
+      threshold: 4
+    });
 		
-		tempSensor.on("change", function(value) {
-			var params = {
-				temperatura: value
-			};
-			dweetio.dweet_for("web-client-iot-cs", params, function(err, dweet){
-			  console.log(dweet.content); 
-			});
-	  });
-
-	});
+    tempSensor.on("change", function(value) {
+      var params = {
+        temperatura: value
+      };
+      dweetio.dweet_for("web-client-iot-cs", params, function(err, dweet){
+        console.log(dweet.content); 
+      });
+    });
+    
+  });
 
 
 
